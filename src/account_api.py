a021969f5e02f9efe1fe04157384c0eaeaea9831
nocyphr <nocphyr@outlook.com>
2022-11-14 17:43:34 +0100

    feat: added skeleton for API calls

---
+++ src/account_api.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+
+
+# @app.get("/balance")
+# def read_root():
+#     return [{"datetime": 12165498},
+#             {"usd-balance": 500},
+#             {"btc-balance": 0.03},
+#             ]
+
+
+# @app.get("/actions")
+# def read_root():
+#     return [{"datetime": 12165498},
+#             {"btc-amount": 0.005},
+#             {"prices": 15000},
+#             ]
+
+
+# @app.get("/transactions")
+# def read_root():
+#     return [{"datetime": 12165498},
+#             {"usd-amount": 50},
+#             {"btc-amount": 0.005},
+#             {"prices": 15000},
+#             ]
